1a3f267a51e41d4ff4c5f145757e3fcf4401a4ef
Grietje Rösler <grietjeroesler@MacBook-Pro-2.local>
2025-09-08 14:42:12 +0200

    added random generation function

--- src/crypt/rand.c
+++ src/crypt/rand.c
@@ -3,5 +3,17 @@
 
 int cr_rand_bytes(unsigned char *buf, size_t len)
 {
-    return -1;
+    FILE *Datei;
+    Datei = fopen ("/dev/urandom", "rb");
+    if(Datei == NULL){
+        return -1;
+    }
+
+    if(fread(buf,1,len,Datei) != len){
+        fclose(Datei);
+        return -1;
+    }
+
+    fclose(Datei);
+    return 0;
 }
